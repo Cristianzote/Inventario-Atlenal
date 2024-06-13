113a43aa0fa8ab2a1fa1943f4b2ea0e104daf308
Cristianzote <125917044+Cristianzote@users.noreply.github.com>
2024-06-13 11:00:23 -0500

    Feat: stats (sales)

---
+++ Access.API/Sales.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Access.API
+{
+    public class Sales
+    {
+        public string[] date { get; set; }
+        public float[] outflow { get; set; }
+        public float[] income { get; set; }
+    }
+}
